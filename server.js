cc3ca42cbbad775aa81ac0a9414dd58129fc0f2f
Reet Priye <reetpriye@gmail.com>
2021-02-23 20:36:41 +0530

    Add user authentication routes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
-const PORT = process.env.port || 5000;
+// Connect to database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('<h1>SplitShare</h1>');
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/data', require('./routes/data'));
+
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
