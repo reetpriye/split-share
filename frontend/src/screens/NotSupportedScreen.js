4fbb616ea1f88b6ab1df11d47490756b6a26d418
reetpriye <reetpriye@gmail.com>
2021-05-11 09:34:00 +0530

    Add svg for larger devices

--- frontend/src/screens/NotSupportedScreen.js
+++ frontend/src/screens/NotSupportedScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import notAvailable from '../assets/not-available.svg'
 import './styles/NotSupported.css'
 
 const NotSupportedScreen = ({ device }) => {
@@ -16,11 +17,13 @@ const NotSupportedScreen = ({ device }) => {
           </>
         ) : (
           <>
-            <h1>
-              Split<span>Share</span>
-            </h1>
-
-            <p>This site is built for mobile devices.</p>
+            <img src={notAvailable} style={{width: '300px'}}/>
+            <div>
+              <h1>
+                Split<span>Share</span>
+              </h1>
+              <p>This site is built for mobile devices.</p>
+            </div>
           </>
         )}
       </div>
